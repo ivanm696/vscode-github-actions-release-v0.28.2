a2fa108d5213974264c850c1b6b32c3172cb1027
ivanm696 <59553806+ivanm696@users.noreply.github.com>
2026-06-17 10:14:31 +0300

    fix(security): The token is passed directly to the Octokit constructor, whi

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,13 +1,1 @@
-import {Octokit} from "@octokit/rest";
-import {version} from "../../package.json";
-import {getGitHubApiUri} from "../configuration/configuration";
-
-export const userAgent = `VS Code GitHub Actions (${version})`;
-
-export function getClient(token: string): Octokit {
-  return new Octokit({
-    auth: token,
-    userAgent: userAgent,
-    baseUrl: getGitHubApiUri()
-  });
-}
+Consider using Octokit's App authentication or Personal Access Token with limited scopes
